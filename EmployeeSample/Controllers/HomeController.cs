1f8a8f28cd3453a8f0636d76d87208549602fafd
agwit.jimmuel@gmail.com <agwit.jimmuel@gmail.com>
2022-11-04 14:03:36 +0800

    Wirth View

--- EmployeeSample/Controllers/HomeController.cs
+++ EmployeeSample/Controllers/HomeController.cs
@@ -3,47 +3,48 @@ using EmployeeSample.Repositories;
 using EmployeeSample.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace EmployeeSample.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IEmployeeRepository _employeeRepository;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IEmployeeRepository _employeeRepository;
 
-        public HomeController(ILogger<HomeController> logger, IEmployeeRepository employeeRepository)
-        {
-            _logger = logger;
-            _employeeRepository = employeeRepository;
-        }
+		public HomeController(ILogger<HomeController> logger, IEmployeeRepository employeeRepository)
+		{
+			_logger = logger;
+			_employeeRepository = employeeRepository;
+		}
 
-        public async Task<IActionResult> IndexAsync()
-        {
-            return View(await _employeeRepository.GetAllAsync());
-        }
+		public async Task<IActionResult> IndexAsync()
+		{
+			return View(await _employeeRepository.GetAllAsync());
+		}
 
-        public async Task<IActionResult> Details(int id)
-        {
-            return View(await _employeeRepository.GetProductById(id));
-        }
-
-
+		//public async Task<IActionResult> Details(int id)
+		//{
+		//	return View(await _employeeRepository.GetProductById(id));
+		//}
 
 
 
 
 
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
 
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+	}
 }
